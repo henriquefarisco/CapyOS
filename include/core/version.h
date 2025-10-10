3076ab9b8c331be2affb75781a28d8cc1378d671
Henrique Schwarz Souza Farisco <contato.schwarz@gmail.com>
2025-10-10 12:32:25 -0300

    chore(release): bump to 0.7.1-alpha.1 and add notes

--- include/core/version.h
+++ include/core/version.h
@@ -3,14 +3,14 @@
 
 #define NOIR_VERSION_MAJOR        0
 #define NOIR_VERSION_MINOR        7
-#define NOIR_VERSION_PATCH        0
+#define NOIR_VERSION_PATCH        1
 
 #define NOIR_VERSION_CHANNEL      "alpha"
 #define NOIR_VERSION_PRERELEASE   "alpha.1"
-#define NOIR_VERSION_EXTENDED     "0.7.0-alpha.1"
-#define NOIR_VERSION_FULL         "0.7.0-alpha.1+20240526"
+#define NOIR_VERSION_EXTENDED     "0.7.1-alpha.1"
+#define NOIR_VERSION_FULL         "0.7.1-alpha.1+20251010"
 
-#define NOIR_VERSION_ALPHA        "0.7.0-alpha.1"
+#define NOIR_VERSION_ALPHA        "0.7.1-alpha.1"
 #define NOIR_VERSION_BETA         "0.6.2-beta.3"
 #define NOIR_VERSION_STABLE       "0.5.4"
 
